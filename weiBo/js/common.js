0a2a6899aae4b26a76333ac52ac621b4cd7ca04a
liuchaojie <liuchaojie@58ganji.com>
2018-08-11 02:21:00 +0800

    micro-blog

---
+++ weiBo/js/common.js
@@ -0,0 +1,12 @@
+function getParamByUrl(paramKey){
+    var url=window.location.search.substring(1);
+    var arr=url.split("&");
+    var result=[];
+    for(var i=0;i < arr.length;i++){
+        var param=arr[i].split("=");
+        if(paramKey === param[0]){
+            return  param[1];
+        }
+    }
+    return "false";
+}
