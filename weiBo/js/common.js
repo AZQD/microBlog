532dd3b7ea737bb9f873c54df14b9fd2b76e55fa
liuchaojie <liuchaojie@58ganji.com>
2018-08-11 15:36:07 +0800

    Formatter

--- weiBo/js/common.js
+++ weiBo/js/common.js
@@ -1,11 +1,11 @@
-function getParamByUrl(paramKey){
-    var url=window.location.search.substring(1);
-    var arr=url.split("&");
-    var result=[];
-    for(var i=0;i < arr.length;i++){
-        var param=arr[i].split("=");
-        if(paramKey === param[0]){
-            return  param[1];
+function getParamByUrl(paramKey) {
+    var url = window.location.search.substring(1);
+    var arr = url.split("&");
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        var param = arr[i].split("=");
+        if (paramKey === param[0]) {
+            return param[1];
         }
     }
     return "false";
