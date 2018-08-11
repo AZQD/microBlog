27b493493d3c3e8ad332924b0dd31c1f7c307ad4
liuchaojie <liuchaojie@58ganji.com>
2018-08-11 15:42:23 +0800

    micro-blog

--- weiBo/js/detail.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    var $tabItem = $('.wrapper .container .detail-main .ctrl-box .menu .tab-item');
-    var $listItem = $('.wrapper .container .detail-main .ctrl-box .showList .item-list');
-    $tabItem.unbind('click').bind('click', function () {
-        var index = $(this).index();
-        $tabItem.removeClass('active').eq(index).addClass('active');
-        $listItem.hide().eq(index).show();
-    });
-});
